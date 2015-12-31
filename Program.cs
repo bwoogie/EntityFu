a5f2409fec4dfc35bd80cec6e24d06a4de78f63c
bwoogie <rockandrollvirus@gmail.com>
2015-12-31 00:22:48 -0600

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -14,8 +14,14 @@ namespace EntityFu {
             EntityFu.Component.numCids = _id; //total number of cids
 
             //Create a new entity and add a HealthComponent to it
-            EntityFu.create(new EntityComponent.HealthComponent(10, 100));
-            EntityFu.create(new EntityComponent.HealthComponent(5, 50));
+            EntityFu.create(
+                new Cid[] {EntityComponent.HealthComponent.cid},
+                new EntityComponent.HealthComponent(10, 100)
+                );
+            EntityFu.create(
+                new Cid[] {EntityComponent.HealthComponent.cid},
+                new EntityComponent.HealthComponent(5, 50)
+                );
 
             //Example usage, take away 1 health point every second.
             while (EntityFu.count() > 0) {
